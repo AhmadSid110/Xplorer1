39b8d4db2ba3016b0abc0bec0c6147a4039e4279
Mohammad Ahmad Siddiqui <ahmad.sid2049@gmail.com>
2026-01-07 15:42:41 +0530

    Add Icons import to FileRow.kt

--- android/app/src/main/java/com/droidexplorer/websim/ui/FileRow.kt
+++ android/app/src/main/java/com/droidexplorer/websim/ui/FileRow.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
